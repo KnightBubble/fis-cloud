2534d2c3f5b9961735758e10a6956ea9ac3dc8d1
zhangying05 <zhangying3210@gmail.com>
2013-12-04 16:05:09 +0800

    添加重启服务器的脚本

--- doc/restart.sh
+++ doc/restart.sh
@@ -1,4 +1,12 @@
-//kill mongodb
-//kill fis-cloud
-//rm -rf mongo.lock
-//start mongo
+#kill mongodb
+#kill fis-cloud
+#rm -rf mongo.lock
+#start mongo
+
+
+#!/usr/bin/env bash
+
+
+
+nohup 	../bin/fis-cloud start --port 8889 >server.log 2>&1 &
+nohup mongod --port 8887 --dbpath=/home/fis-cloud/data/db >mongo.log 2>&1 &
