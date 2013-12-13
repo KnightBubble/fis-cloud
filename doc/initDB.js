b03c605a00d2092de7f6d150a0d63b1f682dd55a
zhangying05 <zhangying3210@gmail.com>
2013-12-13 16:30:44 +0800

    updatae init db file

--- doc/initDB.js
+++ doc/initDB.js
@@ -1,8 +1,17 @@
 //初始化数据库
+//root用户的 _id, name不能修改，password和email可以修改
 
  var fis = require('fis-cloud-kernel');
- var user = {_id:"root", name:"root", _auth:"emhhbmd5aW5nMTExMQ=="};
- fis.db.insert(fis.db.COLLECTION_LIST.user, user, user, {}, function(err, result){
+ var Base64 = require('js-base64').Base64;
+ var user = {_id:"root", name:"root", password:"1111", email:"zhangying3210@gmail.com"};
+ 
+ addUser(user);
+ 
+ function addUser(user){
+ 	user._auth = Base64.encode(user.name + user.password);
+	console.log(user._auth);
+ 	delete user.password;
+ 	fis.db.insert(fis.db.COLLECTION_LIST.user, user, user, {}, function(err, result){
  	if(err){
  		console.log(err);
  		process.exit();
@@ -10,4 +19,6 @@
  		console.log("db init success!");
  		process.exit();
  	}
- });
+	});
+ };
+ 
